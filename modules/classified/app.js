c02f136961e5d8b8ebe62f7eb7448a055495e7a5
David Vasquez <david.vasquez@sundevs.com>
2016-11-01 08:34:11 -0500

    add

--- modules/classified/app.js
+++ modules/classified/app.js
@@ -1,7 +1,7 @@
 (function(){
     var app = angular.module('app',["firebase" ])
     app.controller('appCtrl',function($scope, $firebase){
-    var ref = new Firebase("https://encanto-development.firebaseio.com/classified");
+    var ref = new Firebase("https://encanto-development.firebaseio.com/classified/");
     var sync = $firebase(ref);
     $scope.DB = sync.$asArray();
     $scope.titulo='Clasificados';
@@ -14,8 +14,38 @@
     $scope.delete=function(item){
         $scope.DB.$remove(item);
     };
-    $scope.edit=function(value){
-        $scope.app=value;
+    $scope.ver=function(value){
+            $scope.DB.app(value);
     };
+    
     });
+    
+    
+    
+    app.controller('appView',function($scope, $firebase){
+    var ref = new Firebase("https://encanto-development.firebaseio.com/classified/");
+    var sync = $firebase(ref);
+    $scope.DB = sync.$asArray();
+    $scope.titulo='Clasificados';
+    $scope.app={username:'',country:'', description:'', imageclassified_url:'', timestamp:''};
+
+    $scope.add=function(){
+        $scope.DB.$add($scope.app);
+        $scope.app={username:'',country:'', description:'', imageclassified_url:'', timestamp:''};
+    };
+    $scope.delete=function(item){
+        $scope.DB.$remove(item);
+    };
+    $scope.ver=function(value){
+        if ($scope.DB.$id){
+            var ref = new Firebase("https://encanto-development.firebaseio.com/classified/" + $id);
+            var sync = $firebase(ref);
+            $scope.DB = sync.$asArray();
+            scope.app={username:'',country:'', description:'', imageclassified_url:'', timestamp:''};
+            $scope.DB.app(value);
+        };
+    };
+    
+    });
+   
 })();
--- modules/post/app.js
+++ modules/post/app.js
@@ -1,15 +1,15 @@
 (function(){
     var app = angular.module('app',["firebase" ])
     app.controller('appCtrl',function($scope, $firebase){
-    var ref = new Firebase("https://encanto-development.firebaseio.com/posts");
+    var ref = new Firebase("https://encanto-development.firebaseio.com/posts/");
     var sync = $firebase(ref);
     $scope.DB = sync.$asArray();
     $scope.titulo='Publicaciones';
-    $scope.app={description:'', image_url:'', create_at:''};
+    $scope.app={description:'', image_url:'', create_at:'', comments:''};
 
     $scope.add=function(){
         $scope.DB.$add($scope.app);
-        $scope.app={description:'', image_url:'', create_at:''};
+        $scope.app={description:'', image_url:'', create_at:'', comments:''};
     };
     $scope.delete=function(item){
         $scope.DB.$remove(item);
@@ -21,11 +21,31 @@
     $scope.ver=function(value){
         $scope.app=value;
     };
-    $scope.vercomentarios=function(value){
-        $scope.app=value;
-    };
     
     });
-    
+    /*
+    app.controller('appView',function($scope, $firebase){
+    var ref = new Firebase('https://encanto-development.firebaseio.com/posts/' + $id + '/comments/');
+    var sync = $firebase(ref);
+    $scope.DB = sync.$asArray();
+    $scope.titulo='Publicaciones';
+    $scope.app={comment:'', created_at:'', id:''};
+
+    $scope.add=function(){
+        $scope.DB.$add($scope.app);
+        $scope.app={comment:'', created_at:'', id:''};
+    };
+    $scope.delete=function(item){
+        $scope.DB.$remove(item);
+    };
+    $scope.edit=function(value){
+        $scope.app=value;
+        
+    };
+    $scope.ver=function(value){
+        $scope.app=value;
+    };
+
+    });*/
 })();
 
